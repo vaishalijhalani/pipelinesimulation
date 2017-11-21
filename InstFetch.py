ba3954b027abc8bfbd3f5e520a74be3fb8d79561
vaishali jhalani <vaishalijhalani1@gmail.com>
2017-11-21 17:58:26 +0530

    4commit

--- InstFetch.py
+++ InstFetch.py
@@ -1,4 +1,9 @@
 class Fetch:
 
-   def fetchforfile(self): #fetch instrctions form a file
-      file.readline()
+   def __init__(self,filename): #fetch instrctions form a file
+      self.file = open(filename,"r")
+      #self.object1 = object1
+
+   def next(self): 
+      print self.file.readline() 
+
